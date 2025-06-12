04324826e7a37f7f0e5b045669ea8486895c3e21
iinigmatullin <iinigmatullin@diasoft.ru>
2025-06-12 13:58:48 +0500

    change FileService.java

--- src/main/java/org/example/intershop/service/FileService.java
+++ src/main/java/org/example/intershop/service/FileService.java
@@ -3,22 +3,24 @@ package org.example.intershop.service;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 import java.io.FileNotFoundException;
 
 @Service
 public class FileService {
 
-    public Resource getFile(String fileName) {
-        try {
-            Resource image = new ClassPathResource(String.format("static/%s.jpeg", fileName));
-            if (image.exists()) {
-                return image;
-            } else {
-                throw new FileNotFoundException(String.format("Не удалось найти запрашиваемый файл %s", fileName));
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(String.format("Ошибка при загрузке файла %s", e));
-        }
+    public Mono<Resource> getFile(String fileName) {
+       return Mono.fromSupplier(() -> new ClassPathResource(String.format("static/%s.jpeg", fileName)))
+                .flatMap(resource -> {
+                    if (resource.exists()) {
+                        return Mono.just((Resource) resource);
+                    } else {
+                        return Mono.error(new FileNotFoundException(
+                                String.format("Не удалось найти запрашиваемый файл %s", fileName)
+                        ));
+                    }
+                })
+                .onErrorMap(ex -> new RuntimeException(String.format("Ошибка при загрузке файла %s", fileName)));
     }
 }
