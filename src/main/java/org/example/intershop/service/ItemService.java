f4e4be65423bf0f493c283cb155bfd9ce165096c
iinigmatullin <iinigmatullin@diasoft.ru>
2025-06-12 13:59:03 +0500

    change ItemService.java

--- src/main/java/org/example/intershop/service/ItemService.java
+++ src/main/java/org/example/intershop/service/ItemService.java
@@ -1,61 +1,57 @@
 package org.example.intershop.service;
 
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
-import org.example.intershop.domain.Cart;
-import org.example.intershop.domain.Item;
 import org.example.intershop.dto.ItemDto;
+import org.example.intershop.exception.BusinessException;
 import org.example.intershop.mapper.ItemMapper;
-import org.example.intershop.repository.CartRepository;
 import org.example.intershop.repository.ItemRepository;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
 
     private final ItemRepository itemRepository;
-    private final CartRepository cartRepository;
     private final ItemMapper itemMapper;
 
     @Transactional(readOnly = true)
-    public Page<ItemDto> findAll(PageRequest page, String title) {
+    public Flux<ItemDto> findAll(PageRequest page, String title) {
         if (StringUtils.isBlank(title)) {
-            return itemRepository.findAll(page).map(itemMapper::itemEntityToItemDto);
+            return itemRepository.findAllBy(page)
+                    .map(itemMapper::itemEntityToItemDto);
         }
-        return itemRepository.findAllByTitle(title, page).map(itemMapper::itemEntityToItemDto);
+        return itemRepository.findAllByTitle(title, page)
+                .map(itemMapper::itemEntityToItemDto);
     }
 
     @Transactional(readOnly = true)
-    public ItemDto findById(Long id) {
-        Item item = itemRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("Item with id = %s not found", id)));
-        return itemMapper.itemEntityToItemDto(item);
+    public Mono<ItemDto> findById(Long id) {
+        return itemRepository.findById(id)
+                .map(itemMapper::itemEntityToItemDto)
+                .switchIfEmpty(Mono.error(new BusinessException(String.format("Item with id = %s not found", id))));
     }
 
     @Transactional
-    public ItemDto action(Long id, String action) {
-        Item foundItem = itemRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException(String.format("Item with id = %s not found", id)));
-        Long calculatedCount = calculateCount(action, foundItem.getCount());
-        foundItem.setCount(calculatedCount);
-        Cart cart = cartRepository.findById(1L)
-                .orElseThrow(() -> new EntityNotFoundException("Cart with id = 1 not found"));
-        foundItem.setCart(calculatedCount > 0 ? cart : null);
-        return itemMapper.itemEntityToItemDto(itemRepository.save(foundItem));
+    public Mono<ItemDto> action(Long id, String action) {
+        return itemRepository.findById(id)
+                .switchIfEmpty(Mono.error(new BusinessException(String.format("Item with id = %s not found", id))))
+                .flatMap(foundItem -> {
+                    Long calculatedCount = calculateCount(action, foundItem.getCount());
+                    foundItem.setCount(calculatedCount);
+                    foundItem.setCartId(calculatedCount > 0 ? 1L : null);
+                    return itemRepository.save(foundItem);
+                }).map(itemMapper::itemEntityToItemDto);
     }
 
     @Transactional(readOnly = true)
-    public List<ItemDto> findAllByCartId(Long cartId) {
-        return itemRepository.findAllByCartId(cartId).stream()
-                .map(itemMapper::itemEntityToItemDto)
-                .toList();
+    public Flux<ItemDto> findAllByCartId(Long cartId) {
+        return itemRepository.findAllByCartId(cartId)
+                .map(itemMapper::itemEntityToItemDto);
     }
 
     private Long calculateCount(String action, Long currentCount) {
