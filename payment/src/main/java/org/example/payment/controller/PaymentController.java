4b4cb41ac1f63cbfcb6ae54b59f20f5b686d7136
iinigmatullin <iinigmatullin@diasoft.ru>
2025-06-19 23:47:46 +0500

    Добавить PaymentController.java

---
+++ payment/src/main/java/org/example/payment/controller/PaymentController.java
@@ -0,0 +1,30 @@
+package org.example.payment.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.example.controller.DefaultApi;
+import org.example.payment.service.PaymentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+
+@RestController
+@RequestMapping("/payment")
+@RequiredArgsConstructor
+public class PaymentController implements DefaultApi {
+
+    private final PaymentService paymentService;
+
+    @Override
+    public Mono<ResponseEntity<BigDecimal>> balanceGet(ServerWebExchange exchange) {
+        return null;
+    }
+
+    @Override
+    public Mono<ResponseEntity<BigDecimal>> balancePost(Mono<BigDecimal> body, ServerWebExchange exchange) {
+        return null;
+    }
+}
