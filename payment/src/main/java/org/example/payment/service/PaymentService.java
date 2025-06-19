19b5cf44287a5ed8e8c52a9c5f51055905f1d7f7
iinigmatullin <iinigmatullin@diasoft.ru>
2025-06-19 23:47:57 +0500

    Добавить PaymentService.java

---
+++ payment/src/main/java/org/example/payment/service/PaymentService.java
@@ -0,0 +1,20 @@
+package org.example.payment.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.payment.repository.PaymentRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentService {
+
+    private final PaymentRepository paymentRepository;
+
+    public Mono<BigDecimal> getBalance() {
+        return paymentRepository.getBalance();
+    }
+
+}
