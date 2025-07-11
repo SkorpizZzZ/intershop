c240f7eb30662bf4e09fe77a8846350c51629171
iinigmatullin <iinigmatullin@diasoft.ru>
2025-07-11 20:18:52 +0500

    Добавить кнопку logout

--- store/src/main/java/org/example/intershop/configuration/SecurityConfiguration.java
+++ store/src/main/java/org/example/intershop/configuration/SecurityConfiguration.java
@@ -2,14 +2,17 @@ package org.example.intershop.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.web.server.WebSession;
 
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.net.URI;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -26,7 +29,16 @@ public class SecurityConfiguration {
                         .loginPage("/login")
                         .authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/main"))
                 )
-                .logout(withDefaults())
+                .logout(logout -> logout
+                        .logoutUrl("/logout")
+                        .logoutSuccessHandler((exchange, authentication) -> {
+                            ServerHttpResponse response = exchange.getExchange().getResponse();
+                            response.setStatusCode(HttpStatus.FOUND);
+                            response.getHeaders().setLocation(URI.create("/intershop/login"));
+                            return exchange.getExchange().getSession()
+                                    .flatMap(WebSession::invalidate);
+                        })
+                )
                 .build();
     }
 
